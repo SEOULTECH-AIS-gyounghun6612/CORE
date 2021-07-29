41ac062e99e4a85b857b8c3b8841fc7a7813dd2b
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-07-29 12:00:46 +0900

    file_name change

--- torch_toolbox/setup.py
+++ torch_toolbox/setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_base_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["utils"],
+    packages=["torch_utils"],
     zip_safe=False,
     install_requires=[
         "pytorch", "ais-python-utils"
