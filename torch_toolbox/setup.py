7642e1254a07ec081e36eb7e1a71c028f6f72beb
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-07-29 12:01:37 +0900

    setup link error fix

--- torch_toolbox/setup.py
+++ torch_toolbox/setup.py
@@ -4,7 +4,7 @@ setup(
     name="AIS_torch_utils",
     version="1.0.0",
     description="Custom base code module for python",
-    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_base_utils.git",
+    url="https://github.com/SEOULTECH-AIS-gyounghun6612/torch_base_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=["torch_utils"],
