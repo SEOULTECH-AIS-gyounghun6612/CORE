542401982e22dd0213273ba53f39d19d29fc9e72
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-10-22 16:28:54 +0900

    module rename

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_base_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["ais_python_module"],
+    packages=["python_ex"],
     zip_safe=False,
     install_requires=[
         "opencv-python", "numpy"
