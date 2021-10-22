671f2f033b33e46fb612fb955abacc343b323d9d
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-10-22 16:29:58 +0900

    module rename

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -4,7 +4,7 @@ setup(
     name="AIS_python_utils",
     version="2.1.0",
     description="Custom base code module for python",
-    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_base_utils.git",
+    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_ex_module.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=["python_ex"],
