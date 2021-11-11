01b2b85e4b17547ed0fd15db6348611aa0078d8b
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-11-11 17:47:55 +0900

    update function sobel filter

--- python_toolbox/python_ex/_cv2.py
+++ python_toolbox/python_ex/_cv2.py
@@ -14,6 +14,7 @@ import cv2
 import random
 
 from enum import Enum
+
 if __package__ == "":
     import _base
     import _numpy
@@ -44,6 +45,12 @@ class R_option(Enum):
     ZtoO = 1  # [0, 1.0]
 
 
+class CVT_option(Enum):
+    GRAY2BGR = 0
+    BGR2GRAY = 1
+    BGR2RGB = 2
+
+
 # extention about read and write
 class file():
     # option
@@ -57,6 +64,7 @@ class file():
         if not _base.directory._exist_check(filename, is_file=True):
             _error_message.variable_stop(
                 "file.image_read",
+                ["filename", ],
                 "Have some problem in parameter 'filename'. Not exist")
         # data read
         if color_option == Color_option.BGR:
@@ -169,8 +177,59 @@ class base_process():
             else:
                 return image
 
-    @classmethod
-    def canny(self, gray_image, ths, k_size=3, range=R_option.ZtoM, channel=C_position.Last):
+    @staticmethod
+    def img_cvt(img, cvt_option: CVT_option):
+        if cvt_option == CVT_option.GRAY2BGR:
+            return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        elif cvt_option == CVT_option.BGR2GRAY:
+            return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        elif cvt_option == CVT_option.BGR2RGB:
+            return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    @staticmethod
+    def filtering(image, array):
+        if len(image.shape) > 2:
+            # color image
+            holder = _numpy.image_extention.get_canvus(size=None, sample=image)
+            for _ch_ct in range(image.shape[-1]):
+                holder[:, :, _ch_ct] = base_process.filtering(image[:, :, _ch_ct], array)
+            return holder
+        else:
+            return cv2.filter2D(image, -1, array)
+
+    @staticmethod
+    class blur():
+        default = {
+            "gaussian": {},
+            "bilateral": {
+                "d": -1,
+                "sigmaColor": 5,
+                "sigmaSpace": 5}}
+
+        def __call__(self, image, style):
+            if style == "gaussian":
+                pass
+            elif style == "bilateral":
+                return cv2.bilateralFilter(image, **self.default[style])
+
+
+class edge_process():
+    @staticmethod
+    def sobel(image, threshold=1):
+        if len(image.shape) > 2:
+            # color image
+            holder = _numpy.image_extention.get_canvus(size=image.shape[:2])
+            for _ch_ct in range(image.shape[-1]):
+                holder += edge_process.sobel(image[:, :, _ch_ct])
+            return _numpy.base_process.type_converter(holder >= 2, "uint8")
+        else:
+            dx = cv2.Sobel(image, -1, 1, 0, delta=128)
+            dy = cv2.Sobel(image, -1, 0, 1, delta=128)
+
+            return _numpy.operation.normal_cut(cv2.addWeighted(dx, 0.5, dy, 0.5, 0), threshold, threshold)
+
+    @staticmethod
+    def canny(gray_image, ths, k_size=3, range=R_option.ZtoM, channel=C_position.Last):
         _high = ths[0]
         _low = ths[1]
 
@@ -179,7 +238,7 @@ class base_process():
             [canny_image, canny_image, canny_image],
             channel.value)
 
-        return self.range_converter(canny_image, R_option.ZtoM, range)
+        return base_process.range_converter(canny_image, R_option.ZtoM, range)
 
 
 class gui_process():
@@ -190,8 +249,58 @@ class gui_process():
     def image_cover():
         pass
 
+    # extention display with control
+    class trackbar():
+        pass
+
+    class image_process():
+        pass
+
+    class trackbar_window():
+        pass
+
 
 class draw():
+    @staticmethod
+    def _padding(image, padding):
+        # make holder -> in later add multi padding option
+        if isinstance(padding, int):
+            _t_pad, _b_pad, _l_pad, _r_pad = [padding, padding, padding, padding]
+        elif isinstance(padding, list):
+            if len(padding) == 2:
+                # [hight_pad, weidth_pad]
+                pass
+            elif len(padding) == 4:
+                # [top, bottom, left, right]
+                pass
+
+        if len(image.shape) == 3:
+            _h, _w, _c = image.shape
+            _holder_shape = [_h + (_t_pad + _b_pad), _w + (_l_pad + _r_pad), _c]
+        elif len(image.shape) == 2:
+            _h, _w = image.shape
+            _holder_shape = [_h + (_t_pad + _b_pad), _w + (_l_pad + _r_pad)]
+
+        _holder = _numpy.image_extention.get_canvus(_holder_shape)
+        _holder[_t_pad: -_b_pad, _l_pad: -_r_pad] = image
+
+        return _holder
+
+    @staticmethod
+    def _unpadding(image, padding):
+        # make holder -> in later add multi padding option
+        if isinstance(padding, int):
+            _t_pad, _b_pad, _l_pad, _r_pad = [padding, padding, padding, padding]
+        elif isinstance(padding, list):
+            if len(padding) == 2:
+                # [hight_pad, weidth_pad]
+                pass
+            elif len(padding) == 4:
+                # [top, bottom, left, right]
+                pass
+
+        return image[_t_pad: -_b_pad, _l_pad: -_r_pad]
+
     @staticmethod
     def _text(image, text):
         pass
@@ -216,21 +325,46 @@ class draw():
         else:
             return cv2.polylines(image, pts, is_close, color, thick)
 
+    @staticmethod
+    class canvas():
+        figures = None
 
-# extention display with control
-class trackbar():
-    pass
+        draw_object = []
+        past_points = []  # [x, y]
+        this_point = []  # [x, y]
 
+        draw_pen = {
+            "base_color": [0x00, 0x00, 0x00],
+            "draw_color": [0x00, 0x00, 0x00],
+            "thickness": 3,
+            "style": "solid_line"}
 
-class image_process():
-    pass
+        def clear_canvas(self, size, smaple=None, is_color="white"):
+            self.figures = _numpy.image_extention.get_canvus(size, smaple, is_color)
 
+        def set_draw_pen(self, base, color, thickness, style):
+            self.draw_pen["base_color"] = base
+            self.draw_pen["draw_color"] = color
+            self.draw_pen["thickness"] = thickness
+            self.draw_pen["style"] = style
 
-class trackbar_window():
-    pass
+        def set_object(self, num):
+            pass
+
+        def del_object(self, num):
+            pass
+
+        def clear_object(self):
+            pass
+
+        def draw(self):
+            pass
 
 
 class augmentation():
+    def __call__(self, **option):
+        pass
+
     @staticmethod
     def _make_noise():
         pass
@@ -262,7 +396,7 @@ class augmentation():
     def _flip():
         pass
 
-    def _image_augmentation():
+    def image_augmentation():
         pass
 
 
