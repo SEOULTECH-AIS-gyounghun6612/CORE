45f1db7e85ebfea1cca1ca6ca6bdb86f432f4d04
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-11-15 19:26:59 +0900

    update test version

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="python_ex",
+    name="python_AIS_ex_utils",
     version="2.1.0",
     description="Custom base code module for python",
-    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_ex_module.git",
+    url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_AIS_ex_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["python_ex"],
+    packages=["python_AIS_ex_utils"],
     zip_safe=False,
     install_requires=[
         "opencv-python", "numpy"
