5e16d90876d6a006ce71e68fd46bb08230c975a6
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-12-17 19:37:54 +0900

    directory chagne

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python_AIS_ex_utils",
-    version="2.1.0",
+    version="0.0.1",
     description="Custom base code module for python",
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_AIS_ex_utils.git",
     author="Choi_keonghun & Jun_eins",
