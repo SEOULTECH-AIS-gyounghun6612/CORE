d669e597a47c0c1682604787b1d04504e95f3551
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2021-12-17 19:46:21 +0900

    setup setting error fix

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="python_AIS_ex_utils",
+    name="python_ex",
     version="0.0.1",
     description="Custom base code module for python",
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/python_AIS_ex_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["python_AIS_ex_utils"],
+    packages=["python_ex"],
     zip_safe=False,
     install_requires=[
         "opencv-python", "numpy"
