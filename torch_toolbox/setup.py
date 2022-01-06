71e5baec5f904b5374bbbce7c3deb84d68ef9912
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-01-06 17:19:09 +0900

    bug fix

--- torch_toolbox/setup.py
+++ torch_toolbox/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="torch_AIS_ex_utils",
-    version="0.1.0",
-    description="Custom base code module for python",
+    name="torch_ex",
+    version="0.0.1",
+    description="Custom base code module for pytorch",
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/torch_AIS_ex_utils.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["torch_AIS_ex_utils"],
+    packages=["torch_ex"],
     zip_safe=False,
     install_requires=[
-        "torch", "python_AIS_ex_utils"
+        "torch", "python_ex"
     ]
 )
