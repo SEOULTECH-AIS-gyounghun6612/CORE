37ed44eff97e758240fa61f66a18a90033a3b3e8
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-06-09 19:46:23 +0900

    function Log update

--- python_toolbox/python_ex/_result.py
+++ python_toolbox/python_ex/_result.py
@@ -17,30 +17,43 @@ class logging_option(Enum):
 
 class log():
     def __init__(self, info: Dict = {}, data: Dict = {}, save_dir: str = None, file_name: str = "log.json"):
+        self.save_dir = save_dir
+        self.file_name = file_name
 
-        if save_dir is not None:
-            self.load(save_dir, file_name)
+        _diretory = _base.directory._slash_check(f"{save_dir}/{file_name}", True)
+
+        if _base.file._exist_check(_diretory):
+            self.load()
         else:
             self.info: Dict[str, Union[list, str, int]] = {}
             self.add_info(info)
             self.data: Dict[str, Union[list, dict]] = {}
             self.add_data(self.data, data, logging_option.OVERWRITE)
 
-    def add_info(self, info: Dict, mode: logging_option = logging_option.OVERWRITE):
+    def add_info(self, info: Dict, mode: logging_option = logging_option.OVERWRITE, save_point: Dict = None):
+        if save_point is None:
+            save_point = self.info
+
         for _key in info.keys():
             _flag = _key in self.info.keys() and mode.value  # if added data's key already exist in log's it, check that write mode
-            if _flag:
-                if isinstance(self.info[_key], str):
-                    self.info[_key] = f'{self.info[_key]}\n{info[_key]}'
-                else:
-                    # Due to "ADD" write option, data type must be set "list"
-                    if not isinstance(self.info[_key], list):
-                        self.info[_key] = [self.info[_key], ]
 
-                    self.info[_key].append(info[_key])
+            if isinstance(info[_key], Dict):
+                save_point[_key] = {}
+                self.add_info(info[_key], mode, save_point[_key])
 
             else:
-                self.info[_key] = info[_key]  # False -> (over)write
+                if _flag:
+                    if isinstance(self.info[_key], str):
+                        self.info[_key] = f'{self.info[_key]}\n{info[_key]}'
+                    else:
+                        # Due to "ADD" write option, data type must be set "list"
+                        if not isinstance(self.info[_key], list):
+                            self.info[_key] = [self.info[_key], ]
+
+                        self.info[_key].append(info[_key])
+
+                else:
+                    self.info[_key] = info[_key]  # False -> (over)write
 
     def get_info(self, name: str = None):
         return self.info[name] if name in self.info.keys() else self.info
@@ -64,34 +77,43 @@ class log():
                 else:
                     save_point[_key] = _contents  # False -> (over)write
 
-    def get_data(self, name: Union[List[str], str] = None, serch_point: Dict = None):
+    def get_data(self, name: Union[List[str], str] = None, serch_point: Dict = None, range_st: int = 0, range_ed: int = None) -> Dict:
         serch_point = self.data if serch_point is None else serch_point
-        name = [name, ] if not isinstance(name, list) else name
-        serch_name = name[0]
 
         if name is None:
             # get all data
             return serch_point
-        elif serch_name not in serch_point.keys():
-            # unsuitable data name
-            return None
-        elif isinstance(serch_point[serch_name], Dict) and len(name[2:]):
-            # got ot deep
-            return self.get_data(name[1:], serch_point[serch_name])
-        else:
-            return serch_point[serch_name]
 
-    def load(self, save_dir: str, file_name: str = "log.json"):
-        save_pakage = _base.file._json(save_dir, file_name)
+        name_list = [name, ] if not isinstance(name, list) else name
+        picked_data = {}
+
+        for _name in name_list:
+            if _name not in serch_point.keys():
+                # unsuitable data name
+                ...
+            elif isinstance(serch_point[_name], Dict):
+                # got ot deep
+                picked_data[_name] = self.get_data(name_list[1:], serch_point[_name], range_st, range_ed)
+
+                if not len(picked_data[_name]):
+                    picked_data[_name] = serch_point[_name]
+
+            else:
+                picked_data[_name] = serch_point[_name][range_st: range_ed]
+
+        return picked_data
+
+    def load(self):
+        save_pakage = _base.file._json(self.save_dir, self.file_name)
         self.add_info(save_pakage["info"])
         self.add_data(save_pakage["data"], logging_option.OVERWRITE)
 
-    def save(self, save_dir: str, file_name: str = "log.json"):
+    def save(self):
         save_pakage = {
             "info": self.info,
             "data": self.data}
 
-        _base.file._json(save_dir, file_name, save_pakage, True)
+        _base.file._json(self.save_dir, self.file_name, save_pakage, True)
 
 
 class ploter():
