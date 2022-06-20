9b941c9e511a7679dc1ba91bd0640badba0a01f9
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-06-20 17:08:13 +0900

    make data class

--- python_toolbox/python_ex/_label.py
+++ python_toolbox/python_ex/_label.py
@@ -1,18 +1,18 @@
 from collections import namedtuple
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import Dict, List, Tuple, Union
 
 if __package__ == "":
     # if this file in local project
-    import _base
+    from _base import directory, file
     import _cv2
     import _numpy
     import _error as _e
 
 else:
     # if this file in package folder
-    from . import _base
+    from ._base import directory, file
     from . import _cv2
     from . import _numpy
     from . import _error as _e
@@ -41,6 +41,14 @@ class Label_style(Enum):
     DETECTION = "detection"
 
 
+@dataclass
+class Data():
+    Label_style: Label_style
+    File_style: File_style
+    Input: List = field(default_factory=list)
+    Label: List = field(default_factory=list)
+
+
 class Labels():
     @dataclass
     class basement():
@@ -64,21 +72,27 @@ class Augmentation():
     ...
 
 
-class label_process():
+class Label_process():
     class label_basement():
         Lable_name: str = "basement"
+
         Label_dict: Dict[Label_style, List[Union[Labels.classification_label, Labels.seg_label]]] = {}
         Label_category: Dict[Label_style, List[str]] = {}
 
-        Directory: Dict[Label_style, Dict[File_style, Union[List[str], str]]] = {}
-        Annotation: Dict[Label_style, Union[str, List[str]]] = {}
+        Root_dir: str = directory._make("data", directory._relative_root())
+        Data_directory: Dict[Label_style, Dict[File_style, Union[List[str], str]]] = {}
+        Data_annotation: Dict[Label_style, Union[str, List[str]]] = {}
 
-        def __init__(self, import_style: List[Label_style], data_size: List[int]) -> None:
-            _meta_label: Dict[str, Union[int, str, bool]] = _base.file._json(
-                file_dir=f"{_base.directory._devide(__file__)[0]}/data_file/",
-                file_name=f"{self.Lable_name}.json")
+        Augmentation_list: Augmentation = {}
 
-            for _style in import_style:
+        def __init__(self, import_style: List[Label_style], augmentation: Augmentation, root: str = None) -> None:
+            self.Root_dir = directory._slash_check(root) if root is not None else self.Root_dir  # set data root dir
+
+            _meta_label: Dict[str, Union[int, str, bool]] = file._json(
+                file_dir=f"{directory._devide(__file__)[0]}/data_file/",
+                file_name=f"{self.Lable_name}.json")  # get label information from file
+
+            for _style in import_style:  # data set in label dictionary from meta label
                 if _style == Label_style.CLASSIFICATION:
                     _label_list: List[Labels.classification_label] = [Labels.classification_label(**data) for data in _meta_label[_style.value]]
 
@@ -90,36 +104,65 @@ class label_process():
         def set_learning_mode(self, learning_mode: str):
             self.learning_mode = learning_mode
 
-        def make_file_process_profile(self, label_style: Label_style, file_style: File_style) -> Dict:
-            ...
+        def get_data_profile(self, IO_label_style: Label_style, IO_file_style: File_style) -> Data:
+            data_profile = Data(Label_style=IO_label_style, File_style=IO_file_style, Input=[], Label=[])
 
-        def work(self, data_list, index):
+            return data_profile
+
+        def work(self, data: Data, index: int):
             ...
 
     class Imagenet_1k(label_basement):
         Directory: Dict[Label_style, Dict[File_style, Union[List[str], str]]] = {
             Label_style.CLASSIFICATION: {
-                File_style.IMAGE_FILE: "ILSVRC/{}/{}",
-                File_style.ZIP_FILE: "ILSVRC/",
-                File_style.ANNOTATION: "ILSVRC/"}
+                File_style.IMAGE_FILE: "ILSVRC/2012/{}/{}/",
+                File_style.ZIP_FILE: "ILSVRC/2012/",
+                File_style.ANNOTATION: "ILSVRC/2012/"}
         }
         Annotation: Dict[Label_style, Dict[File_style, Union[List[str], str]]] = {
             Label_style.CLASSIFICATION: {
+                File_style.IMAGE_FILE: None,
                 File_style.ZIP_FILE: "{}_map_for_zip.txt",
                 File_style.ANNOTATION: "{}_map_for_annotation.json"}
         }
 
-        def __init__(self, import_style: List[Label_style], data_size: List[int]) -> None:
+        def __init__(self, import_style: List[Label_style], augmentation: Augmentation, root: str = None) -> None:
             self.Lable_name = "imagenet_1k"
-            super().__init__(import_style, data_size)
+            super().__init__(import_style, augmentation, root)
 
-        def make_file_process_profile(self, label_style: Label_style, file_style: File_style) -> Dict:
-            if file_style == File_style.IMAGE_FILE:
-                return {"directory": self.Directory[label_style][file_style], "label_list": self.Label_dict[label_style], "annotation_file_name": None}
-            elif file_style == File_style.ANNOTATION:
-                return {"directory": self.Directory[label_style][file_style], "label_list": None, "annotation_file_name": self.Annotation[label_style][file_style]}
+        def get_data_profile(self, IO_label_style: Label_style, IO_file_style: File_style) -> Data:
+            data_profile = super().get_data_profile(IO_label_style, IO_file_style)
 
-        def work(self, data_list, index):
+            IO_file_dir = self.Directory[IO_label_style][IO_file_style]
+            IO_anno = self.Annotation[IO_label_style][IO_file_style]
+
+            # make data list
+            if data_profile.File_style == File_style.IMAGE_FILE:
+                _label_list = self.Label_dict[IO_label_style]
+                _class_info = [[_label.Class_name, _label.Train_ID] for _label in _label_list]
+
+                for _name, train_id in _class_info:
+                    _file_list = directory._inside_search(self.Root_dir + IO_file_dir.format(self.learning_mode, _name))
+                    data_profile.Input += _file_list
+                    data_profile.Label += [train_id for _ in range(len(_file_list))]
+
+            elif data_profile.File_style == File_style.ANNOTATION:
+                _annotation = file._json(self.Root_dir + IO_file_dir, IO_anno.format(self.learning_mode))
+                data_profile.Input += _annotation["input"]
+                data_profile.Label += _annotation["label"]
+
+            return data_profile
+
+        def work(self, data: Data, index):
+            _label_list = self.Label_dict[data.Label_style]
+
+            if data.File_style == File_style.IMAGE_FILE:
+                picked_image = _cv2.file.image_read(data.Input[index])
+                picked_image = _cv2.cv_base.resize(picked_image, [224, 224])
+                picked_label = _numpy.np_base.get_array_from(len(_label_list), True, dtype=_numpy.np_base.np_dtype.np_float32)
+
+                return [picked_image, picked_label]
+                ...
             ...
 
     class Imagenet_22k(label_basement):
@@ -144,10 +187,10 @@ class label_process():
             self.Lable_name = "BDD-100k"
             super().__init__(import_style, data_size)
 
-        def make_file_process_profile(self, label_style: Label_style, file_style: File_style) -> Dict:
+        def get_data_profile(self, holder: Data) -> Data:
             ...
 
-        def work(self, data_list, index):
+        def work(self, data, index):
             ...
 
     class CDnet():
@@ -169,54 +212,12 @@ class label_process():
             self.Lable_name = "CDnet"
             super().__init__(import_style, data_size)
 
-        def make_file_process_profile(self, label_style: Label_style, file_style: File_style) -> Dict:
+        def get_data_profile(self, holder: Data) -> Data:
             ...
 
-        def work(self, data_list, index):
+        def work(self, data, index):
             ...
 
     class COCO():
         def __init__(self) -> None:
             pass
-
-
-class file_process():
-    class basement():
-        def __init__(self, root_directory: str, label_style: Label_style, file_style: File_style) -> None:
-            self.root = _base.directory._slash_check(root_directory)
-            self.label_style = label_style
-            self.file_style = file_style
-
-        def get_profile_option(self):
-            return {"label_style": self.label_style, "file_style": self.file_style}
-
-        def work(self, directory: str, **info):
-            _dir = _base.directory._slash_check(self.root + directory)
-            data_list = {"input": [], "label": [], "file_style": self.file_style}
-
-            return _dir, data_list
-
-    class classification(basement):
-        def __init__(self, root_directory: str, file_style: File_style = File_style.IMAGE_FILE) -> None:
-            super().__init__(root_directory, file_style)
-
-        def work(self, directory: str, label_list: List[Labels.classification_label], annotation_file_name: str):
-            _dir, data_list = super().work(directory)
-
-            if self.file_style == File_style.IMAGE_FILE:
-                _class_info = [[_label.Class_name, _label.Train_ID] for _label in label_list]
-
-                for _name, train_id in _class_info:
-                    _file_list = _base.directory._inside_search(_dir.format(_name))
-                    data_list["input"] += _file_list
-                    data_list["label"] += [train_id for _ in range(len(_file_list))]
-
-            elif self.file_style == File_style.ANNOTATION:
-                _annotation = _base.file._json(_dir, annotation_file_name)
-                data_list["input"] = _annotation["input"]
-                data_list["label"] = _annotation["label"]
-
-            return _dir, data_list
-
-    class segmentation(basement):
-        ...
