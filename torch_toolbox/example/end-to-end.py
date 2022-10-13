217c47962c7fa78b4a19563ed260ac37fb679449
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-10-13 13:32:58 +0900

    example  update

--- torch_toolbox/example/end-to-end.py
+++ torch_toolbox/example/end-to-end.py
@@ -7,13 +7,16 @@ from torch_ex._structure import backbone
 
 # directory
 Data_root = "/home/ais-choi-01/DATA/Data/"
-Result_root = directory._make_for_result(folder="../result_example/", root_dir=directory._relative_root())
 
 # Learning_opt
 learning_opt = opt._learning.E2E(
     # Infomation about train
+    Learing_name="example",
     Learning_date=utils.time_stemp(is_text=True),
     Learning_detail="",
+
+    Save_root=directory._relative_root(),
+
     Learning_mode=[learing_mode.VALIDATION],
 
     # About Learning type and style
@@ -28,26 +31,34 @@ learning_opt = opt._learning.E2E(
 dataloader_opt = opt._dataloader(
     Batch_size=16,
     Num_workers=8,
-    Data_process=Label_process.BDD_100k(import_style=[Label_style.SEM_SEG, ], data_size=[224, 224], root=Data_root),
+    Data_process=Label_process.BDD_100k(import_style=[Label_style.SEM_SEG, ], data_size=[224, 224], root="/home/ais-choi-01/DATA/Data/"),
     Data_label_style=Label_style.SEM_SEG,
     Data_file_style=File_style.IMAGE_FILE)
 
 
 class custom(Learning_process.End_to_End):
+    def __init__(self, learning_opt: opt._learning.E2E, dataloader_opt: opt._dataloader, log_file: str = None, is_restore: bool = False) -> None:
+        super().__init__(learning_opt, dataloader_opt, log_file, is_restore)
+
+        learning_block = [backbone.fcn(structure_opt.backbone(101)), structure_opt.optim("Adam")]
+
+        # set model n optim
+        self.save_learning_moduel([learning_block, ], is_restore)
+
     def data_jump_to_gpu(self, data_list):
         return super().data_jump_to_gpu(data_list)
 
-    def fit(self, epoch: int = 0, mode: str = "train", is_display: bool = False, save_root: str = None):
+    def fit(self, epoch: int = 0, mode: learing_mode = learing_mode.TRAIN, is_display: bool = False, save_root: str = None):
+        for _input, _label in self.dataloaders[mode]:
+            if self.learning_opt.Use_cuda:
+                ...
         return super().fit(epoch, mode, is_display, save_root)
 
     def result_save(self, mode: str, epoch: int, save_root: str):
         return super().result_save(mode, epoch, save_root)
 
 
-process = custom(Result_root, learning_opt=learning_opt, dataloader_opt=dataloader_opt)
-_model = backbone.fcn(structure_opt.backbone(101))
-_optim = structure_opt.optim("Adam")
-
-process.set_model_n_optim(_model, _optim)
+process = custom(learning_opt=learning_opt, dataloader_opt=dataloader_opt)
+process.fit(mode=learing_mode.VALIDATION)
 
 print("end")
