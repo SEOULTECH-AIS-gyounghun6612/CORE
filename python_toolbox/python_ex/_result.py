aabc6a6421cb3de50fa6dca5fc0c0fef5657a88e
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-10-23 21:03:05 +0900

    Function update; naming rule update

--- python_toolbox/python_ex/_result.py
+++ python_toolbox/python_ex/_result.py
@@ -10,29 +10,29 @@ else:
     from . import _base
 
 
-class logging_option(Enum):
+# -- DEFINE CONSTNAT -- #
+class Logging_option(Enum):
     ADD = True
     OVERWRITE = False
 
 
-class log():
-    def __init__(self, info: Dict = {}, data: Dict = {}, save_dir: str = None, file_name: str = "log.json", is_resotre: bool = False):
-        self.save_dir = save_dir
-        self.file_name = file_name
+class Log():
+    _Annotation: Dict[str, Union[list, str, int]] = {}
+    _Data: Dict[str, Union[list, dict]] = {}
 
-        _diretory = _base.Directory._slash_check(f"{save_dir}{file_name}", True)
+    def __init__(self, info: Dict = {}, data: Dict = {}, save_dir: str = None, file_name: str = "log.json"):
+        self._Save_dir = save_dir
+        self._File_name = file_name
 
-        if _base.File._exist_check(_diretory) and is_resotre:
-            self.load()
+        if _base.File._exist_check(_base.Directory._slash_check(f"{save_dir}{file_name}", True)):
+            self._load()
         else:
-            self.annotation: Dict[str, Union[list, str, int]] = {}
-            self.add(info)
-            self.data: Dict[str, Union[list, dict]] = {}
-            self.add(data, logging_option.OVERWRITE, self.data)
+            self._add(info)
+            self._add(data, Logging_option.OVERWRITE, self._Data)
 
-    def add(self, block: Dict, mode: logging_option = logging_option.ADD, save_point: Dict = None):
+    def _add(self, block: Dict, mode: Logging_option = Logging_option.ADD, save_point: Dict = None):
         if save_point is None:
-            save_point = self.annotation
+            save_point = self._Annotation
 
         for _key in block.keys():
             _flag = _key in save_point.keys() and mode.value  # if added data's key already exist in log's it, check that write mode
@@ -40,13 +40,13 @@ class log():
             if isinstance(block[_key], Dict):
                 if not _flag:
                     save_point[_key] = {}
-                self.add(block[_key], mode, save_point[_key])
+                self._add(block[_key], mode, save_point[_key])
 
             else:
                 # add
                 if _flag and mode.value:
                     if isinstance(save_point[_key], str):
-                        save_point[_key] = f'{self.annotation[_key]}\n{block[_key]}'
+                        save_point[_key] = f'{self._Annotation[_key]}\n{block[_key]}'
                     else:
                         # if save_point is not a list, convert that to list for add new data.
                         if not isinstance(save_point[_key], list):
@@ -61,46 +61,46 @@ class log():
                 else:
                     save_point[_key] = block[_key]
 
-    def get_annotation(self, name: str = None) -> Dict:
-        return self.annotation[name] if name in self.annotation.keys() else self.annotation
+    def _get_annotation(self, name: str = None) -> Dict:
+        return self._Annotation[name] if name in self._Annotation.keys() else self._Annotation
 
-    def get_data(self, name: Union[List[str], str] = None, serch_point: Dict = None, range_st: int = 0, range_ed: int = None) -> Dict[str, list]:
-        serch_point = self.data if serch_point is None else serch_point
+    def _get_data(self, name: Union[List[str], str] = None, serch_point: Dict = None, range_st: int = 0, range_ed: int = None) -> Dict[str, list]:
+        serch_point = self._Data if serch_point is None else serch_point
 
         if name is None:
             # get all data
             return serch_point
 
-        name_list = [name, ] if not isinstance(name, list) else name
+        __name_list = [name, ] if not isinstance(name, list) else name
         picked_data = {}
 
-        for _name in name_list:
-            if _name not in serch_point.keys():
+        for __name in __name_list:
+            if __name not in serch_point.keys():
                 # unsuitable data name
                 ...
-            elif isinstance(serch_point[_name], Dict):
+            elif isinstance(serch_point[__name], Dict):
                 # got ot deep
-                picked_data[_name] = self.get_data(name_list[1:], serch_point[_name], range_st, range_ed)
+                picked_data[__name] = self._get_data(__name_list[1:], serch_point[__name], range_st, range_ed)
 
-                if not len(picked_data[_name]):
-                    picked_data[_name] = serch_point[_name]
+                if not len(picked_data[__name]):
+                    picked_data[__name] = serch_point[__name]
 
             else:
-                picked_data[_name] = serch_point[_name][range_st: range_ed]
+                picked_data[__name] = serch_point[__name][range_st: range_ed]
 
         return picked_data
 
-    def load(self):
-        save_pakage = _base.File._json(self.save_dir, self.file_name)
-        self.add(save_pakage["annotation"])
-        self.add(save_pakage["data"], self.data)
+    def _load(self):
+        save_pakage = _base.File._json(self._Save_dir, self._File_name)
+        self._add(save_pakage["annotation"])
+        self._add(save_pakage["data"], self._Data)
 
-    def save(self):
+    def _save(self):
         save_pakage = {
-            "annotation": self.annotation,
-            "data": self.data}
+            "annotation": self._Annotation,
+            "data": self._Data}
 
-        _base.File._json(self.save_dir, self.file_name, save_pakage, True)
+        _base.File._json(self._Save_dir, self._File_name, save_pakage, True)
 
 
 class ploter():
