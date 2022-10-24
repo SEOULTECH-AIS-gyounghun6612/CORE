c576802d841386eacedd82f90e7db43a3990cc10
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-10-24 15:55:24 +0900

    function update; log

--- python_toolbox/python_ex/_result.py
+++ python_toolbox/python_ex/_result.py
@@ -12,8 +12,9 @@ else:
 
 # -- DEFINE CONSTNAT -- #
 class Logging_option(Enum):
-    ADD = True
-    OVERWRITE = False
+    ADD = 0
+    READDING = 1
+    DELETE = 2
 
 
 class Log():
@@ -27,73 +28,63 @@ class Log():
         if _base.File._exist_check(_base.Directory._slash_check(f"{save_dir}{file_name}", True)):
             self._load()
         else:
-            self._add(info)
-            self._add(data, Logging_option.OVERWRITE, self._Data)
+            self._insert(info)
+            self._insert(data, self._Data)
 
-    def _add(self, block: Dict, mode: Logging_option = Logging_option.ADD, save_point: Dict = None):
-        if save_point is None:
-            save_point = self._Annotation
+    def _insert(self, data_block: Dict, access_point: Dict = None, is_overwrite: bool = True):
+        # check parameter; save point
+        if access_point is None:
+            access_point = self._Annotation
 
-        for _key in block.keys():
-            _flag = _key in save_point.keys() and mode.value  # if added data's key already exist in log's it, check that write mode
+        # pick data in search point
+        for __key in data_block.keys():
+            __key_exist = __key in access_point.keys()
 
-            if isinstance(block[_key], Dict):
-                if not _flag:
-                    save_point[_key] = {}
-                self._add(block[_key], mode, save_point[_key])
+            if isinstance(data_block[__key], dict):
+                if not __key_exist:
+                    access_point[__key] = {}  # Make new key in holder that not be exist key
+                self._insert(data_block[__key], access_point[__key], is_overwrite)  # go to deep
 
             else:
                 # add
-                if _flag and mode.value:
-                    if isinstance(save_point[_key], str):
-                        save_point[_key] = f'{self._Annotation[_key]}\n{block[_key]}'
+                if __key_exist and not is_overwrite:
+                    if isinstance(access_point[__key], str):
+                        access_point[__key] = f'{access_point[__key]}\n{data_block[__key]}'
+                    elif isinstance(access_point[__key], list):
+                        access_point[__key].append(data_block[__key])
                     else:
-                        # if save_point is not a list, convert that to list for add new data.
-                        if not isinstance(save_point[_key], list):
-                            save_point[_key] = [save_point[_key], ]
-
-                        if not isinstance(block[_key], list):
-                            save_point[_key].append(block[_key])
-                        else:
-                            save_point[_key] += block[_key]
-
+                        access_point[__key] = [access_point[__key], ]
+                        access_point[__key].append(data_block[__key])
                 # (over)write
                 else:
-                    save_point[_key] = block[_key]
+                    access_point[__key] = data_block[__key]
 
-    def _get_annotation(self, name: str = None) -> Dict:
-        return self._Annotation[name] if name in self._Annotation.keys() else self._Annotation
+    def _get(self, holder: Dict, access_point: Dict = None, is_pop: bool = True):
+        # check parameter; save point
+        if access_point is None:
+            access_point = self._Annotation
 
-    def _get_data(self, name: Union[List[str], str] = None, serch_point: Dict = None, range_st: int = 0, range_ed: int = None) -> Dict[str, list]:
-        serch_point = self._Data if serch_point is None else serch_point
+        # pick data in search point
+        for __key in holder.keys():
+            __key_exist = __key in access_point.keys()
 
-        if name is None:
-            # get all data
-            return serch_point
-
-        __name_list = [name, ] if not isinstance(name, list) else name
-        picked_data = {}
-
-        for __name in __name_list:
-            if __name not in serch_point.keys():
-                # unsuitable data name
-                ...
-            elif isinstance(serch_point[__name], Dict):
-                # got ot deep
-                picked_data[__name] = self._get_data(__name_list[1:], serch_point[__name], range_st, range_ed)
-
-                if not len(picked_data[__name]):
-                    picked_data[__name] = serch_point[__name]
-
-            else:
-                picked_data[__name] = serch_point[__name][range_st: range_ed]
-
-        return picked_data
+            if __key_exist:
+                if isinstance(holder[__key], dict):
+                    self._get(holder[__key], access_point[__key], is_pop)  # go to deep
+                elif isinstance(holder[__key], list):
+                    holder[__key] = access_point[__key][holder[__key][0]: holder[__key][1]]
+                    if is_pop:
+                        del access_point[__key][holder[__key][0]: holder[__key][1]]
+                    ...
+                else:
+                    holder[__key] = access_point[__key]
+                    if is_pop:
+                        del access_point[__key]
 
     def _load(self):
         save_pakage = _base.File._json(self._Save_dir, self._File_name)
-        self._add(save_pakage["annotation"])
-        self._add(save_pakage["data"], self._Data)
+        self._insert(save_pakage["annotation"])
+        self._insert(save_pakage["data"], self._Data)
 
     def _save(self):
         save_pakage = {
