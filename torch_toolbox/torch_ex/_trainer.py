538bffaf3e2d36852af46ac8274fa65fe954c7b3
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-11-10 17:29:48 +0900

    fix

--- torch_toolbox/torch_ex/_trainer.py
+++ torch_toolbox/torch_ex/_trainer.py
@@ -174,8 +174,8 @@ class Learning_process():
                     dataset=self._Dataset[_mode],
                     batch_size=int(self._Config._Batch_size / word_size),
                     num_workers=int(self._Config._Num_workers / word_size),
-                    sampler=_sampler[_mode])
-
+                    sampler=_sampler[_mode],
+                    shuffle=_mode==Learning_Mode.TRAIN)
             return _sampler, _dataloader
 
         def _set_learning_model(self, this_gpu: int = 0, is_cuda: bool = False) -> Tuple[Custom_Model, Optimizer, _LRScheduler]:
