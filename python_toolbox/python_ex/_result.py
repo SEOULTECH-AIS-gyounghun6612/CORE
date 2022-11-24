14cbb65519497d913c25ec2970849c76a92d00b3
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2022-11-24 20:13:39 +0900

    fix

--- python_toolbox/python_ex/_result.py
+++ python_toolbox/python_ex/_result.py
@@ -65,52 +65,42 @@ class Log():
 
     def _get_data(
             self,
-            data_info: Dict[str, Optional[Union[str, List[str], Dict]]],
+            data_info: Optional[Dict[str, Optional[Union[str, Dict]]]],
             access_point: Dict[str, JSON_WRITEABLE],
             is_pop: bool = False) -> Dict[str, JSON_WRITEABLE]:
 
+        # truth table(row: _picked_data, col: _tag_info, overwrite is False)
+        #                 not dict                     dict
+        #   None     pick_selected_data         pick_selected_data
+        #   str              x            pick_data in selected_data[str]
+        #   dict             x                       go to deep
+
         _holder = {}
 
-        for _naem_tag, _tag_info in data_info.items():
-            if _naem_tag not in access_point.keys():
-                continue
-            _selected_data = access_point[_naem_tag]
-
-            # truth table(row: _picked_data, col: _tag_info, overwrite is False)
-            #                 not dict                     dict
-            #   None     pick_selected_data         pick_selected_data
-            #   str              x            pick_data in selected_data[str]
-            # list(str)          x            pick_data in selected_data[str]
-            #   dict             x                       go to deep
-
-            if _tag_info is not None:
-                if not isinstance(_selected_data, dict):
+        if data_info is None:
+            _holder.update(dict((_key, access_point.pop(_key) if is_pop else _data) for _key, _data in access_point.items()))
+        else:
+            for _data_name, _tag_info in data_info.items():
+                if _data_name not in access_point.keys():
                     continue
-                elif isinstance(_tag_info, dict):
-                    _holder = dict((
-                        "_".join([_naem_tag, _getting_key]),
-                        _getting_value) for _getting_key, _getting_value in self._get_data(_tag_info, _selected_data, is_pop).items())
-
+                _access_point = access_point[_data_name]
+                if _tag_info is None or not isinstance(_access_point, dict):
+                    _pick = _access_point
+                    if is_pop:
+                        del _access_point
+                    _holder.update({_data_name: _pick})
+                elif isinstance(_tag_info, str):
+                    _pick = _access_point.pop(_tag_info) if is_pop else _access_point[_tag_info]
+                    _holder.update({_data_name: _pick})
                 else:
-                    if isinstance(_tag_info, str):
-                        _holder.update(
-                            {"_".join([_naem_tag, _tag_info]): _selected_data.pop(_tag_info) if is_pop else _selected_data[_tag_info]})
-                    elif isinstance(_tag_info, list):
-                        _holder.update(dict((
-                            "_".join([_naem_tag, _call_key]),
-                            _selected_data.pop(_call_key) if is_pop else _selected_data[_call_key]) for _call_key in _tag_info))
-            else:
-                _holder.update({_naem_tag: _selected_data})
-
-                if is_pop:
-                    del access_point[_naem_tag]
-
+                    _pick = self._get_data(_tag_info, _access_point, is_pop)
+                    _holder.update(dict((f"{_data_name}_{_key}", _value) for _key, _value in _pick.items()))
         return _holder
         # in later change this function => return dict or list
 
     def _get_length(
             self,
-            data_info: Dict[str, Optional[Union[str, List[str], Dict]]],
+            data_info: Optional[Dict[str, Optional[Union[str, Dict]]]],
             access_point: Dict[str, JSON_WRITEABLE]):
 
         _data = self._get_data(data_info, access_point)
