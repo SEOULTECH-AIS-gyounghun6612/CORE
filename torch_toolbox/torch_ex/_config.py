97cc60917f679a8ab5e1a2e5597aae69b7be6f6c
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2023-01-10 10:09:43 +0900

    make new module "config"

--- torch_toolbox/torch_ex/_config.py
+++ torch_toolbox/torch_ex/_config.py
@@ -1,22 +1,24 @@
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Tuple, Optional
 from dataclasses import dataclass, field
 
 from torch import cuda
-from python_ex._base import Directory, Utils, JSON_WRITEABLE
+from python_ex._base import Directory, File, Utils, JSON_WRITEABLE
 
 
 if __package__ == "":
     # if this file in local project
-    from torch_ex._torch_base import Learning_Mode, Process_Type
-    from torch_ex._trainer import Multi_Method
+    from torch_ex._torch_base import Learning_Mode, Parameter_Type
+    from torch_ex._process import Multi_Method
+    from torch_ex._label import Support_Label, Label_Style, File_Style, Label
+    from torch_ex._dataset import Supported_Transform, Supported_Augment, Augment
+    from torch_ex._optimizer import Suport_Optimizer, Suport_Schedule
 else:
     # if this file in package folder
-    from ._torch_base import Learning_Mode, Process_Type
-    from ._trainer import Multi_Method
-    ...
-
-
-# -- DEFINE CONFIG -- #
+    from ._torch_base import Learning_Mode, Parameter_Type
+    from ._process import Multi_Method
+    from ._label import Support_Label, Label_Style, File_Style, Label
+    from ._dataset import Supported_Transform, Supported_Augment, Augment
+    from ._optimizer import Suport_Optimizer, Suport_Schedule
 
 
 # -- Mation Function -- #
@@ -24,111 +26,195 @@ class Learning_Config:
     @dataclass
     class E2E(Utils.Config):
         # Infomation about learning
-        _Project_Name: str = "End_to_End_learning"
-        _Detail: str = "Empty"
-        _Date: str = Utils.Time._apply_text_form(Utils.Time._stemp(), True, "%Y-%m-%d")
-        _Save_root: str = Directory._relative_root()
+        project_name: str = "End_to_End_learning"
+        description: str = "Empty"
+
+        # _date: str = Utils.Time._apply_text_form(Utils.Time._stemp(), True, "%Y-%m-%d")
+        save_root: str = Directory._relative_root()
 
         # About Learning type and style
-        _Batch_size_in_node: int = 4
-        _Max_num_workers: int = 2
+        max_epoch: int = 100
+        last_epoch: int = -1
+        learning_mode: List[str] = field(default_factory=lambda: [Learning_Mode.TRAIN.value, Learning_Mode.VALIDATION.value])
 
-        _Max_epochs: int = 100
-        _Last_epoch: int = -1
-        _Learning_list: List[Learning_Mode] = field(default_factory=lambda: [Learning_Mode.TRAIN, Learning_Mode.VALIDATION])
+        batch_size_per_node: int = 4
+        num_worker_per_node: int = 2
 
+        gpu_ids: List[int] = field(default_factory=lambda: list(range(cuda.device_count())))
 
-@dataclass
-class Hardware_Config(Utils.Config):
-    # about use GPU
-    _GPU_list: List[int] = field(default_factory=lambda: list(range(cuda.device_count())))
+        world_size: int = 1
+        this_rank: int = 1
+        multi_method: str = Multi_Method.DDP.value
+        multi_protocal: Optional[str] = "tcp://127.0.0.1:10001"
 
-    # about multi process
-    _Multi_protocol: Multi_Method = Multi_Method.NONE
+        def _get_parameter(self) -> Dict[str, Any]:
+            _param = super()._get_parameter()
 
-    # about distribute
-    _Num_of_global_node: int = 1
-    _This_computer_rank: int = 0
-    _Host_address: str = "tcp://127.0.0.1:10001"
+            _param["learning_mode"] = [Learning_Mode(_value) for _value in self.learning_mode]
 
-    def _get_parameter(self) -> Dict[str, Any]:
-        return {
+            _param["multi_method"] = Multi_Method(_param["multi_method"])
 
-        }
+            if _param["multi_method"] is Multi_Method.NONE:
+                _param["world_size"] = 1
+            else:
+                if self.this_rank * cuda.device_count() > self.world_size:
+                    _param["world_size"] = self.this_rank * cuda.device_count()
 
-    def _convert_to_dict(self) -> Dict[str, JSON_WRITEABLE]:
-        return super()._convert_to_dict()
+            return _param
+
+    @staticmethod
+    def _Build(meta_data: Dict[str, Any]) -> Utils.Config:
+        _learing_type = meta_data["type"]
+        _learing_parameter = meta_data["parameter"]
+
+        return Learning_Config.__dict__[_learing_type](**_learing_parameter)
 
 
 @dataclass
 class Tracker_Config(Utils.Config):
     # Logging parameter in each mode;
-    _Tracking_pram: Dict[Learning_Mode, Dict[Process_Type, List[str]]]  # str -> logging_loss name
+    # Learning_Mode -> Parameter_Type -> obj_name
+    tracking: Dict[str, Dict[str, List[str]]] = field(default_factory=dict)
 
     # Observe parameter in each mode;
-    # if None -> all same like logging keys
-    _Observing: Dict[Learning_Mode, Dict[Process_Type, Optional[List[str]]]] = field(default_factory=dict)  # str -> logging_loss name
+    # Learning_Mode -> Parameter_Type -> obj_name
+    observing: Dict[str, Dict[str, Optional[List[str]]]] = field(default_factory=dict)
 
-    def _get_parameter(self):
+    def _get_parameter(self) -> Dict[str, Any]:
         return {
-            "tracking_param": self._Tracking_pram,
-            "observing_param": self._Observing}
-
-    def _convert_to_dict(self):
-        return {
-            "_Tracking": dict((
-                _mode_key.value,
-                dict((
-                    _process_key.value,
-                    dict((_name, None) for _name in _name_info)
-                ) for _process_key, _name_info in _process_info.items())
-            ) for _mode_key, _process_info in self._Tracking_pram.items()),
-            "_Observing": dict((
-                _mode_key.value,
-                dict((
-                    _process_key.value,
-                    None if _name_info is None else dict((_name, None) for _name in _name_info)
-                ) for _process_key, _name_info in _process_info.items())
-            ) for _mode_key, _process_info in self._Observing.items())}
+            "tracking_param": dict((
+                Learning_Mode(_mode),
+                dict((Parameter_Type(_process), _obj) for _process, _obj in _tracking_info.items())) for _mode, _tracking_info in self.tracking.items()),
+            "observing_param": dict((
+                Learning_Mode(_mode),
+                dict((Parameter_Type(_process), _obj) for _process, _obj in _observing_info.items())) for _mode, _observing_info in self.observing.items())
+        }
 
 
 @dataclass
-class Augment_Config(Utils.Config):
-    ...
+class Dataset_Config(Utils.Config):
+    label_name: str  # Support_Label
+    label_style: List[str]  # List[Label_Style]
+
+    file_info: Dict[str, List[Tuple[str, str, Optional[Any]]]]  # Dict[Learning_Mode, ]
+
+    amplification: Dict[str, int]  # Dict[Learning_Mode, int]
+
+    transform_list: Dict[str, Dict[str, Dict[str, JSON_WRITEABLE]]]  # Dict[Learning_Mode, Dict[Tr_name, Parameter dict]]
+
+    meta_file: Optional[str] = None
+    data_root: str = "./data/"
+    agment_method: str = Supported_Augment.ALBUIMIENTATIONS.value  # Supported_Augment
+    agment_option: Optional[Dict[str, JSON_WRITEABLE]] = None
+
+    def _get_parameter(self) -> Dict[str, Any]:
+        return {
+            "label_process": Label.Process.__dict__[self.label_name](
+                Support_Label(self.label_name),
+                [Label_Style(_style_name) for _style_name in self.label_style],
+                self.meta_file),
+            "file_profiles": dict((
+                Learning_Mode(_mode_name),
+                [Label.File_IO.__dict__[self.label_name](
+                    (Label_Style(_label_style), File_Style(_file_style), _optional),
+                    self.data_root
+                ) for _label_style, _file_style, _optional in _data_list]
+            ) for _mode_name, _data_list in self.file_info.items()),
+            "amplification": dict((
+                Learning_Mode(_mode_name),
+                _value
+            )for _mode_name, _value in self.amplification.items()),
+            "augmentation": dict((
+                Learning_Mode(_mode_name),
+                Augment.__dict__[self.agment_method]([Supported_Transform.__dict__[_tr_name](**_tr_param) for _tr_name, _tr_param in _tr_dict.items()], **self.agment_option)
+            ) for _mode_name, _tr_dict in self.transform_list.items())
+        }
 
 
 @dataclass
-class Scheduler_Config(Utils.Config):
-    _Optim_name: Suport_Optimizer
-    _Schedule_name: Suport_Schedule
+class Optim_n_Schedule_Config(Utils.Config):
+    optim_name: str  # Suport_Optimizer
+    schedule_name: str  # Suport_Schedule
 
-    _LR_Maximum: float = 0.005
-    _LR_Minimum: float = 0.0001
-    _LR_Decay: float = 1.0
+    initial_lr: float = 0.0005
+    maximum_lr: float = 0.0001
+    minimum_lr: float = 0.00001
+    lr_decay: float = 0.99
 
-    _Term: Union[List[int], int] = 50
-    _Term_amp: float = 1.0
+    schedule_term: int = 10
+    schedule_term_amp: int = 1
 
-    def _get_parameter(self, model: Custom_Model) -> Dict[str, Any]:
+    def _get_parameter(self) -> Dict[str, Any]:
         return {
-            "optimizer": optim.__dict__[self._Optim_name.value](model.parameters(), self._LR_Maximum),
-            "schedule_name": self._Schedule_name,
-            "term": self._Term,
-            "term_amp": self._Term_amp,
-            "maximum": self._LR_Maximum,
-            "minimum": self._LR_Minimum,
-            "decay": self._LR_Decay}
+            "optim_name": Suport_Optimizer(self.optim_name),
+            "initial_lr": self.initial_lr,
 
-    def _convert_to_dict(self) -> Dict[str, Union[Dict, str, int, float, bool, None]]:
-        return {
-            "_Optim_name": self._Optim_name.value,
-            "_Schedule_name": self._Schedule_name.value,
-            "_LR_Maximum": self._LR_Maximum,
-            "_LR_Minimum": self._LR_Minimum,
-            "_LR_Decay": self._LR_Decay,
-            "_Term": self._Term,
-            "_Term_amp": self._Term_amp}
+            "schedule_name": Suport_Schedule(self.schedule_name),
+            "term": self.schedule_term,
+            "term_amp": self.schedule_term_amp,
+            "maximum": self.maximum_lr,
+            "minimum": self.minimum_lr,
+            "decay": self.lr_decay
+        }
 
 
-def _file_to_config(file_dir: str):
-    ...
+class Config():
+    def __init__(self, file_name: str, file_dir: str = "./config/"):
+        self._Load(file_name, file_dir)
+
+    # Freeze function
+    def _Save(self, file_name: str, file_dir: str = "./config/"):
+        _, _file_name = File._extension_check(file_name, ["json"], True)
+        _file_dir = Directory._divider_check(file_dir)
+
+        File._json(_file_dir, _file_name, True, self._config)
+
+    def _Load(self, file_name: str, file_dir: str = "./config/"):
+        _, _file_name = File._extension_check(file_name, ["json"], True)
+        _file_dir = Directory._divider_check(file_dir)
+
+        _config_file = f"{_file_dir}{_file_name}"
+
+        if File._exist_check(_config_file):
+            # load config
+            self._config: Dict[str, Any] = File._json(_file_dir, _file_name)
+        else:
+            self._config = {}
+            print(f"config file {_config_file} not exsit. \nyou must config data initialize before use it")
+
+    def _Get_learning_config(self):
+        return Learning_Config._Build(self._config["learning"])._get_parameter()
+
+    def _Set_learning_config(self, learning_config: Learning_Config.E2E):
+        self._config.update({"learning": {
+            "type": learning_config.__class__.__name__,
+            "parameter": learning_config._convert_to_dict()
+        }})
+
+    def _Get_tracker_config(self):
+        return Tracker_Config(**self._config["tracker"])._get_parameter()
+
+    def _Set_tracker_config(self, tracker_config: Tracker_Config):
+        self._config.update({"tracker": tracker_config._convert_to_dict()})
+
+    def _Get_dataset_config(self):
+        return Dataset_Config(**self._config["dataset"])._get_parameter()
+
+    def _Set_dataset_config(self, dataset_config: Dataset_Config):
+        self._config.update({"dataset": dataset_config._convert_to_dict()})
+
+    def _Get_optim_n_shedule_config(self):
+        return Optim_n_Schedule_Config(**self._config["optim_n_shedule"])._get_parameter()
+
+    def _Set_optim_n_shedule_config(self, optim_n_shedule_config: Optim_n_Schedule_Config):
+        self._config.update({"optim_n_shedule": optim_n_shedule_config._convert_to_dict()})
+
+    def _Set_model_structure_config(self, model_config: Utils.Config):
+        self._config.update({"model_config": {
+            "type": model_config.__class__.__name__,
+            "parameter": model_config._convert_to_dict()
+        }})
+
+    # Un-Freeze function
+    def _Get_model_structure_config(self):
+        raise NotImplementedError
