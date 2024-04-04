40ce32be1920d2141be40ab261adc48504462560
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-04-04 17:52:18 +0900

    apply pylint

--- torch_toolbox/torch_ex/augmentation/__init__.py
+++ torch_toolbox/torch_ex/augmentation/__init__.py
@@ -6,7 +6,6 @@ def Build(augment_name: str, augment_config: Dict[str, Any], process_config: Dic
         from .From_Torchvision import FromTorchvision
         return FromTorchvision(**augment_config).Config_to_compose(process_config)
 
-    else:
-        _error_text = f"This augment process, that from {augment_name.lower()}, is not suport in this module version.\n"
-        _error_text = f"{_error_text}Please change the augment process or module version\n"
-        raise ValueError(_error_text)
+    _error_text = f"This augment process, that from {augment_name.lower()}, is not suport in this module version.\n"
+    _error_text = f"{_error_text}Please change the augment process or module version\n"
+    raise ValueError(_error_text)
