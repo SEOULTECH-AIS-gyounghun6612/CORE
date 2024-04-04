bfacad393911c74d9335c209b518cceb007b8a98
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-04-04 18:03:23 +0900

    bug fix

--- torch_toolbox/setup.py
+++ torch_toolbox/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 requirments_package = [
@@ -19,7 +19,7 @@ setup(
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/AIS_torch_ex.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["torch_ex"],
+    packages=find_packages(),
     package_data=package_opt,
     python_requires=">=3.10.0",
     install_requires=requirments_package,
