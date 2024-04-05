120b1c9a3e9605bcf0dfcfe3852156452c046f5d
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-04-05 10:16:34 +0900

    setup.py update

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 
 requirments_package = [
@@ -18,7 +18,7 @@ setup(
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/AIS_python_ex.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["python_ex"],
+    packages=find_namespace_packages(),
     python_requires=">= 3.10.0",
     dependency_links=dependency_links,
     install_requires=requirments_package,
