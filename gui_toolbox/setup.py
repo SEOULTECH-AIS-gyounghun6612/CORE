7d34eb460b6830ad66c9612e9351560f374d36ab
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-06-19 19:42:02 +0900

    update setup

--- gui_toolbox/setup.py
+++ gui_toolbox/setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
-    name="pyqt5_ex",
+    name="gui_ex",
     version="0.0.1",
     description="",
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/pyqt5_ex_module.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
-    packages=["GUI_ex"],
+    packages=find_namespace_packages(),
     zip_safe=False,
     python_requires=">= 3.7.0",
     install_requires=[
