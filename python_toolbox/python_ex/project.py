82b44378156618913e10c702ceb0f551e6814b79
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-08-06 15:42:36 +0900

    remove data block.

--- python_toolbox/python_ex/project.py
+++ python_toolbox/python_ex/project.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 from typing import (
-    Any, Literal, Type, TypeVar, Generic
+    Any
 )
 from dataclasses import asdict, dataclass
 
-from .system import Path, File, Time, String
+from .system import Path, File, Time
 
 
 @dataclass
@@ -35,210 +35,6 @@ class Config():
     @staticmethod
     def Read_from(file_name: str, file_dir: str):
         return File.Json.Read(file_name, file_dir)
-
-
-class Data_n_Block():
-    @dataclass
-    class Numbered_Data():
-        id_num: int
-
-        def Convert_save_format(
-            self,
-            additional: dict[str, str] | None = None,
-            slot_length: dict[str, int] | None = None
-        ) -> dict[str, str]:
-            _data: dict[str, str] = {}
-
-            if slot_length is None or "id_num" not in slot_length:
-                _key = "id_num"
-                _value = str(self.id_num)
-            else:
-                _key, _value = String.Str_adjust_with_key(
-                    "id_num",
-                    str(self.id_num),
-                    slot_length["id_num"]
-                )
-            _data.update({_key: _value})
-
-            if additional is not None:
-                _data.update(additional)
-
-            return _data
-
-        def __eq__(self, other):
-            if isinstance(other, self.__class__):
-                _t_dict = self.__dict__
-                _o_dict = other.__dict__
-                if len(_t_dict) != len(_o_dict):
-                    # option for "other come from child class"
-                    # when is okay, change to "pass" or "..."
-                    return False
-
-                _t_hash = sum([
-                    hash(_v) for _k, _v in _t_dict.items() if _k != "id_num"
-                ])
-                _o_hash = sum([
-                    hash(_v) for _k, _v in _o_dict.items() if _k != "id_num"
-                ])
-
-                return _t_hash == _o_hash
-            return False
-
-        def __ne__(self, other):
-            return not self.__eq__(other)
-
-
-    NUMED_DATA = TypeVar(
-        "NUMED_DATA",
-        bound=Numbered_Data
-    )
-
-    @dataclass
-    class Numbered_String_Data():
-        _id_num: str
-
-        def Convert_save_format(
-            self,
-            additional: dict[str, str] | None = None,
-            slot_length: dict[str, int] | None = None
-        ) -> dict[str, str]:
-            _data: dict[str, str] = {}
-
-            if slot_length is None or "_id_num" not in slot_length:
-                _key = "_id_num"
-                _value = self._id_num
-            else:
-                _key, _value = String.Str_adjust_with_key(
-                    "_id_num",
-                    self._id_num,
-                    slot_length["_id_num"]
-                )
-            _data.update({_key: _value})
-
-            if additional is not None:
-                _data.update(additional)
-
-            return _data
-
-        def __eq__(self, other):
-            if isinstance(other, self.__class__):
-                _t_dict = self.__dict__
-                _o_dict = other.__dict__
-                if len(_t_dict) != len(_o_dict):
-                    # option for "other come from child class"
-                    # when is okay, change to "pass" or "..."
-                    return False
-
-                _t_hash = sum([
-                    hash(_v) for _k, _v in _t_dict.items() if _k != "_id_num"
-                ])
-                _o_hash = sum([
-                    hash(_v) for _k, _v in _o_dict.items() if _k != "_id_num"
-                ])
-
-                return _t_hash == _o_hash
-            return False
-
-        def __ne__(self, other):
-            return not self.__eq__(other)
-
-        @property
-        def id_num(self):
-            return int(self._id_num)
-
-        @id_num.setter
-        def id_num(self, id_num: int):
-            self._id_num = str(id_num)
-
-    NUMED_STRING_DATA = TypeVar(
-        "NUMED_STRING_DATA",
-        bound=Numbered_String_Data
-    )
-
-    class Block_Of_Numbered_String(Generic[NUMED_STRING_DATA]):
-        def __init__(
-            self,
-            data_type: Type[Data_n_Block.NUMED_STRING_DATA],
-            file_name: str = "data",
-            file_dir: str = Path.WORK_SPACE,
-        ) -> None:
-            _file_path = Path.Join(file_name, file_dir)
-
-            self.data_type = data_type
-
-            self.data_dict: dict[int, data_type] = {}
-            self.last_id = 0
-
-            if Path.Exist_check(_file_path):
-                self.Read_from_csv(file_name, file_dir)
-
-            self.last_id = max(self.data_dict) if len(self.data_dict) else -1
-
-        def Read_from_csv(
-            self,
-            file_name: str,
-            file_dir: str
-        ):
-            _data_type = self.data_type
-            _data_holder = self.data_dict
-
-            for _data in File.CSV.Read_from_file(file_name, file_dir):
-                _comp: Data_n_Block.NUMED_STRING_DATA = _data_type(**_data)
-                _data_holder[int(_data["id_num"])] = _comp
-
-        def Write_to_csv(
-            self,
-            file_name: str,
-            file_dir: str,
-            data_socket_size: dict[str, int] | None = None
-        ):
-            _data_dict = self.data_dict
-
-            if not _data_dict:
-                return False
-
-            return File.CSV.Write_to_file(
-                file_name,
-                file_dir,
-                [
-                    _data.Convert_save_format(
-                        slot_length=data_socket_size
-                    ) for _data in _data_dict.values()
-                ],
-                list(self.data_type.__annotations__.__dict__)
-            )
-
-        def Update_data(
-            self,
-            new_data: Data_n_Block.NUMED_STRING_DATA,
-            is_override: bool = False
-        ) -> bool:
-            if isinstance(new_data, self.data_type):
-                _data_id = new_data.id_num
-                if is_override:
-                    self.data_dict[int(_data_id)] = new_data
-                    return True
-                elif new_data not in self.data_dict.values():  # add
-                    _this_id = self.last_id + 1
-                    new_data.id_num = _this_id
-                    self.data_dict[_this_id] = new_data
-                    self.last_id += 1
-                    return True
-                else:
-                    raise ValueError(
-                        f"This is already in {self.__class__.__name__} block")
-            return False
-
-        def Get_data_from(self, id_num: int, is_pop: bool = False):
-            if id_num in self.data_dict:
-                if is_pop:
-                    return True, self.data_dict.pop(id_num)
-                return True, self.data_dict[id_num]
-            return False, None
-
-        def Clear_data(self):
-            self.data_dict = {}
-            self.last_id = 0
 
 
 class Template():
@@ -297,3 +93,51 @@ class Template():
             ]
         )
         self.result_root = Path.Make_directory(_result_dir_dirs)
+
+
+class Debuging():
+    """
+    프로젝트 진행에 따른 실행 내역 및 결과와 같은 주요 내용을 생성, 출력, 기록하기 위한 모듈
+
+    --------------------------------------------------------------------
+    """
+
+    @staticmethod
+    def Progress_bar(
+        iteration: int,
+        total: int,
+        prefix: str = '',
+        suffix: str = '',
+        decimals: int = 1,
+        length: int = 100,
+        fill: str = '█'
+    ):
+        """
+        Call in a loop to create terminal progress bar
+
+        Parameters
+        --------------------
+        iteration
+            current iteration
+        total
+            total iterations (Int)
+        prefix
+            prefix string (Str)
+        suffix
+            suffix string (Str)
+        decimals
+            positive number of decimals in percent complete (Int)
+        length
+            character length of bar (Int)
+        fill
+            bar fill character (Str)
+        """
+        percent = (
+            "{0:." + str(decimals) + "f}"
+        ).format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end="\r")
+        # Print New Line on Complete
+        if iteration == total:
+            print()
