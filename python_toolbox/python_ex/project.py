d80c822634a757b6d2f5ca3507157b5671c98251
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2024-12-12 15:02:53 +0900

    name fix

--- python_toolbox/python_ex/project.py
+++ python_toolbox/python_ex/project.py
@@ -100,7 +100,7 @@ class Project_Template(Generic[CFG]):
 
     """
     def __init__(self, config_template: type[CFG]):
-        self.project_CFG: CFG = self.Get_config(config_template)
+        self.project_cfg: CFG = self.Get_config(config_template)
         self.save_root = self.Make_save_root()
 
     def Get_args_by_arg_parser(self) -> argparse.ArgumentParser:
