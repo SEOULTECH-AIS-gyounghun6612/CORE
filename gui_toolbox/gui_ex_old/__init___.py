1b34900fd983c80fa6bb29abb608db9530f735cb
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-02-21 11:34:07 +0900

    delete old code

--- gui_toolbox/gui_ex_old/__init___.py
+++
@@ -1,8 +0,0 @@
-if __package__ == "":
-    # if this module call to local for another project
-    import sys
-    from os import path
-
-    # add file dir
-    if path.dirname(path.abspath(__file__)) not in sys.path:
-        sys.path.append(path.dirname(path.abspath(__file__)))
