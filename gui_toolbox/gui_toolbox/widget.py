1bdbcdfed335b8e2cc5330893895ddbe11a6ed5b
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-02-26 17:31:10 +0900

    신규 widget; 편집 가능한 tab

--- gui_toolbox/gui_toolbox/widget.py
+++ gui_toolbox/gui_toolbox/widget.py
@@ -5,7 +5,9 @@ import numpy as np
 from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtCore import Signal  # , Qt
 
-from PySide6.QtWidgets import (QFrame, QLabel, QWidget)
+from PySide6.QtWidgets import (
+    QFrame, QLabel, QWidget, QTabBar, QTabWidget, QLineEdit
+)
 # from PySide6.QtWebEngineWidgets import QWebEngineView
 
 
@@ -59,3 +61,71 @@ class Image_Display_Widget(QLabel):
         _h, _w = img.shape[:2]
         self.setPixmap(QPixmap.fromImage(QImage(img.data, _w, _h, _format)))
         return True
+
+
+class Editable_TabBar(QTabBar):
+    tab_name_change: Signal = Signal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.name_edit = QLineEdit(self)
+        self.name_edit.setVisible(False)
+        self.name_edit.editingFinished.connect(self.Changed_name)
+        self.current_edit_index = None
+
+    def mouseDoubleClickEvent(self, event):
+        index = self.tabAt(event.pos())
+        if index < 0 or self.tabText(index) == "+":
+            return
+        self.current_edit_index = index
+
+        self.name_edit.setGeometry(self.tabRect(index))
+        self.name_edit.setText(self.tabText(index))
+        self.name_edit.setVisible(True)
+        self.name_edit.setFocus()
+        self.name_edit.selectAll()
+
+    def Changed_name(self):
+        if self.current_edit_index is not None:
+            new_text = self.name_edit.text()
+            self.setTabText(self.current_edit_index, new_text)
+        self.name_edit.setVisible(False)
+        self.current_edit_index = None
+
+
+class Editable_Tab(QTabWidget):
+    def __init__(
+        self,
+        tab_prefix: str,
+        parent: QWidget | None = None
+    ) -> None:
+        super().__init__(parent)
+        self.tab_prefix = tab_prefix
+
+        self.setTabBar(Editable_TabBar(self))
+        self.setTabsClosable(True)
+
+        self.addTab(QWidget(), "+")
+        self.tabBar().setTabButton(
+            0, QTabBar.ButtonPosition.RightSide, None)
+
+        self.tabCloseRequested.connect(self._Close_tab)
+        self.tabBarClicked.connect(self._Handle_tab_click)
+
+    def _Make_new_tab(self, **kwarg) -> tuple[str, QWidget]:
+        raise NotImplementedError
+
+    def Add_new_tab(self, **kwarg):
+        _tab_name, _tab_widget = self._Make_new_tab(**kwarg)
+        _this_index = self.count() - 1  # 마지막 '+' 탭 앞에 삽입
+        self.insertTab(_this_index, _tab_widget, _tab_name)
+        self.setCurrentIndex(_this_index)
+
+    def _Close_tab(self, index):
+        if index == self.count() - 1:
+            return
+        self.removeTab(index)
+
+    def _Handle_tab_click(self, index):
+        if index == self.count() - 1:
+            self.Add_new_tab()
