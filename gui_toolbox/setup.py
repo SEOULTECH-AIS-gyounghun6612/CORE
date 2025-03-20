0d5d30c5244448b38ee0ff41b77a0b75e5f867cb
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-03-20 15:35:14 +0900

    update base setting

--- gui_toolbox/setup.py
+++ gui_toolbox/setup.py
@@ -8,11 +8,11 @@ setup(
     name="gui_toolbox",
     version="0.0.1",
     description="",
-    url="https://github.com/SEOULTECH-AIS-gyounghun6612/pyqt5_ex_module.git",
+    url="https://github.com/SEOULTECH-AIS-gyounghun6612/AIS_gui_toolbox.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=find_namespace_packages(),
     zip_safe=False,
-    python_requires=">= 3.7.0",
+    python_requires=">= 3.10.0",
     install_requires=requirements_package
 )
