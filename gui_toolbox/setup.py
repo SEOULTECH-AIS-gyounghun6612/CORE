94ae7e06ff70469f3273c050f18de5dde90bfa83
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-09-02 20:10:34 +0900

    update setup

--- gui_toolbox/setup.py
+++ gui_toolbox/setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_namespace_packages
 
-
-def load_requirements(filename="requirements.txt"):
-    with open(filename, encoding="utf-8") as f:
-        return [_l for _l in f.readlines() if _l and not _l[0] != "#"]
-
 setup(
     name="gui_toolbox",
     version="0.0.1",
@@ -15,5 +10,4 @@ setup(
     packages=find_namespace_packages(),
     zip_safe=False,
     python_requires=">= 3.10.0",
-    install_requires=load_requirements()
 )
