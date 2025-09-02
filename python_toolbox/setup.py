aed0502b270514803b247fc27d3a96a1e5320495
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-09-02 20:10:30 +0900

    update setup

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_namespace_packages
 
-
-def load_requirements(filename="requirements.txt"):
-    with open(filename, encoding="utf-8") as f:
-        return [_l for _l in f.readlines() if _l and not _l[0] != "#"]
-
-
 setup(
     name="python_ex",
     version="1.1.0",
@@ -15,6 +9,5 @@ setup(
     author_email="dev.gyounghun6612@gmail.com",
     packages=find_namespace_packages(),
     python_requires=">= 3.12.0",
-    install_requires=load_requirements(),
     zip_safe=False
 )
