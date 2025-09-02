81e9b7ccc8b9229a6afe8a49d61fea6e5a814425
SEOULTECH-AIS-gyounghun6612 <keonghun6612@seoultech.ac.kr>
2025-09-02 20:14:21 +0900

    버전 매칭

--- python_toolbox/setup.py
+++ python_toolbox/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="python_ex",
-    version="1.1.0",
+    version="1.0",
     description="Custom base code module for python",
     url="https://github.com/SEOULTECH-AIS-gyounghun6612/AIS_python_ex.git",
     author="Choi_keonghun & Jun_eins",
     author_email="dev.gyounghun6612@gmail.com",
     packages=find_namespace_packages(),
-    python_requires=">= 3.12.0",
+    python_requires=">= 3.11.0",
     zip_safe=False
 )
